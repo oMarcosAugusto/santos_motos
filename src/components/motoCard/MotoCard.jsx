7535d73c420c951210d6ca46cd687dce47efc3c4
oMarcosAugusto <marcoosaugusto@hotmail.com>
2024-07-10 20:23:36 +0100

    Realizado alguns ajustes no card e aplicado CSS module ao MotoCard

--- src/components/motoCard/MotoCard.jsx
+++ src/components/motoCard/MotoCard.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import "./MotoCard.css";
+import style from "./MotoCard.module.css";
 import ImageCarousel from "./ImageCarousel";
 
 const MotoCard = ({ cards }) => {
   return (
-    <div className="container_cards">
-      <div className="info_new">
+    <div className={style.container_cards}>
+      <div className={style.info_new}>
         <h1>Novidades</h1>
       </div>
 
       {cards.map((card) => (
-        <div key={card.id} className="card">
+        <div key={card.id} className={style.card}>
           <ImageCarousel
             images={Array.isArray(card.url) ? card.url : [card.url]}
           />
-          <ul className="list_info">
-            <li className="info_preco">{card.preco}</li>
-            <li className="info_ano_km">
-              <span className="info_ano">{card.ano}</span>
-              <span className="info_km">{card.km}</span>
+          <ul className={style.list_info}>
+            <li className={style.info_preco}>{card.preco}</li>
+            <li className={style.info_ano_km}>
+              <span className={style.info_ano}>{card.ano}</span>
+              <span className={style.info_km}>{card.km}</span>
             </li>
           </ul>
-          <div className="btn-container">
-            <button className="btn">Saiba Mais</button>
+          <div className={style.btnContainer}>
+            <button className={style.btn}>Saiba Mais</button>
           </div>
         </div>
       ))}
